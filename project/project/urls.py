1c98153a4e1fc4da22c2704780b625b96b49091e
mikhail <mishaprizby@gmail.com>
2024-01-24 16:03:48 +0300

    added front

--- project/project/urls.py
+++ project/project/urls.py
@@ -45,16 +45,16 @@ urlpatterns = [
     path("api/v1/storage/", include("storage.urls")),
     path("api/v1/generator/", include("taste_generator.urls")),
     re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
+        r"^api/v1/swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
     re_path(r"^api/v1/auth/", include("djoser.urls")),
     re_path(r"^api/v1/auth/", include("djoser.urls.authtoken")),
     path(
-        "swagger/",
+        "api/v1/swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/v1/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

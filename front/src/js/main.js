d1b3471e6f010260b0a992a177996468c1c98cf4
mikhail <mishaprizby@gmail.com>
2024-01-31 21:57:51 +0300

    Added front

--- front/src/js/main.js
+++ front/src/js/main.js
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         password: password
       };
 
-      fetch('/api/v1/auth/token/login', {
+      fetch('http://188.68.221.107/api/v1/auth/token/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -110,42 +110,91 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 // -----------------Склад_____________________
 
-
-
+let organization;
 // Функция для получения данных с сервера Get
 function fetchData() {
-  fetch('/api/v1/storage/tobacco/')
+  fetch('http://188.68.221.107/api/v1/storage/tobacco/')
     .then(response => response.json())
     .then(data => {
       updateTable(data);
+
+      // Предполагаем, что `data` является массивом объектов, как в вашем примере
+      data.forEach(item => {
+        if(item.organization) { // Проверяем, существует ли свойство organization в каждом объекте
+          organization = item.organization; // Сохраняем значение organization из объекта
+          console.log('Organization:', organization); // Выводим значение для проверки
+        }
+      });
     })
     .catch(error => {
       console.error('Error fetching data: ', error);
     });
 }
+
+
 function toggleEditDeleteButtons(id) {
   const editDeleteButtons = document.getElementById(`edit-delete-${id}`);
-  editDeleteButtons.style.display = editDeleteButtons.style.display === 'none' ? 'block' : 'none';
+  editDeleteButtons.style.display = editDeleteButtons.style.display === 'none' ? 'flex' : 'none';
+  toggleRowStyle(id); 
 }
+
+
+
+// ------------Меняем цвет строки_______________________
+function toggleRowStyle(id) {
+  const row = document.querySelector(`tr[data-id="${id}"]`);
+  if (row.classList.contains('highlighted')) {
+    row.classList.remove('highlighted');
+  } else {
+    row.classList.add('highlighted');
+  }
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
+  const year = date.getFullYear();
+
+  return `${day}.${month}.${year}`;
+}
+
+
 function updateTable(data) {
   const tableBody = document.querySelector('.stock__table tbody');
   tableBody.innerHTML = '';
   data.forEach((item, index) => {
+    // Преобразовать даты перед отображением
+    const formattedPurchaseDate = formatDate(item.purchase_date);
+    const formattedBestBeforeDate = formatDate(item.best_before_date);
+
     const row = `<tr data-id="${item.id}">
-    <td>${index + 1}</td>
-    <td>${item.brand}</td>
-    <td>${item.supplier}</td>
-    <td>${item.taste}</td>
-    <td>${item.taste_group}</td>
-    <td>${item.purchase_date}</td>
-    <td>${item.best_before_date}</td>
-    <td>${item.weight}</td>
-    <td>${item.price}</td>
-    <td class="actions"><div class="action-buttons"><button class="options-button" onclick="toggleEditDeleteButtons(${item.id})">...</button><div class="edit-delete-buttons" id="edit-delete-${item.id}" style="display: none;"><button class="edit-button" data-id="${item.id}">Редактировать</button><button class="delete-button" data-id="${item.id}">Удалить</button></div></div></td></tr>`;
+      <td>${index + 1}</td>
+      <td>${item.brand}</td>
+      <td>${item.supplier}</td>
+      <td>${item.taste}</td>
+      <td>${item.taste_group}</td>
+      <td>${formattedPurchaseDate}</td>
+      <td>${formattedBestBeforeDate}</td>
+      <td>${item.weight}</td>
+      <td>${item.price}</td>
+      <td class="actions">
+        <div class="action-buttons">
+          <button class="options-button" onclick="toggleEditDeleteButtons(${item.id})">...</button>
+          <div class="edit-delete-buttons" id="edit-delete-${item.id}" style="display: none;">
+            <button class="edit-button" data-id="${item.id}">Редактировать</button>
+            <button class="delete-button" data-id="${item.id}">Удалить</button>
+          </div>
+        </div>
+      </td>
+    </tr>`;
     tableBody.innerHTML += row;
   });
   document.querySelector('.stock__box').style.display = data.length === 0 ? 'flex' : 'none';
 }
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   fetchData();
   document.querySelector('.stock__table').addEventListener('click', function(e) {
@@ -167,23 +216,56 @@ function editItem(id) {
 // Новая функция для начала редактирования
 function startEditing(id) {
   const row = document.querySelector(`tr[data-id="${id}"]`);
+  if (!row) {
+    console.error('Строка с id не найдена:', id);
+    return;
+  }
   const cells = row.querySelectorAll('td');
+  row.style.height = '100px';
 
   // Превращаем каждую ячейку в редактируемое поле, кроме первой и последней (индекс и кнопки действий)
   for (let i = 1; i < cells.length - 1; i++) {
     const input = document.createElement('input');
     input.type = 'text';
     input.value = cells[i].innerText;
+    input.classList.add('reset-default-styles');
     cells[i].innerText = '';
     cells[i].appendChild(input);
   }
 
-  // Заменяем кнопки на кнопку "Сохранить"
+  // Добавляем кнопку "Сохранить"
   const saveButton = document.createElement('button');
   saveButton.textContent = 'Сохранить';
   saveButton.onclick = () => saveEdit(id);
-  cells[cells.length - 1].innerHTML = '';
-  cells[cells.length - 1].appendChild(saveButton);
+
+  // Добавляем кнопку "Отменить"
+  const cancelButton = document.createElement('button');
+  cancelButton.textContent = 'Отменить';
+  cancelButton.onclick = () => cancelEdit(id);
+
+  // Получаем контейнер для кнопок редактирования и удаления
+  const buttonsContainer = document.getElementById(`edit-delete-${id}`);
+  if (buttonsContainer) {
+    buttonsContainer.innerHTML = ''; // Очищаем контейнер перед добавлением новых кнопок
+    buttonsContainer.appendChild(saveButton);
+    buttonsContainer.appendChild(cancelButton);
+  } else {
+    console.error('Элемент для редактирования не найден:', `edit-delete-${id}`);
+  }
+}
+
+function toISOFormat(dateString) {
+  // Разбиваем строку "дд.мм.гггг" на части
+  const parts = dateString.split('.');
+  const day = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1; // Месяц в JavaScript начинается с 0
+  const year = parseInt(parts[2], 10);
+
+  // Создаем объект Date
+  const date = new Date(Date.UTC(year, month, day));
+
+  // Возвращаем дату в формате ISO 8601
+  return date.toISOString();
 }
 
 // Функция для сохранения изменений
@@ -191,20 +273,26 @@ function saveEdit(id) {
   const row = document.querySelector(`tr[data-id="${id}"]`);
   const inputs = row.querySelectorAll('input');
   const updatedItem = {
-    brand: inputs[0].value, // Предположим, что это brand
-    supplier: inputs[1].value, // Предположим, что это supplier
+    brand: inputs[0].value, 
+    supplier: inputs[1].value, 
     taste: inputs[2].value,
     taste_group: inputs[3].value,
-    purchase_date: inputs[4].value,
-    best_before_date: inputs[5].value,
+    purchase_date: toISOFormat(inputs[4].value), // Преобразуем дату в ISO
+    best_before_date: toISOFormat(inputs[5].value), // Преобразуем дату в ISO
     weight: inputs[6].value,
     price: inputs[7].value,
     
     // ... получить остальные значения
+    
   };
-
+  if (row) {
+    // Восстановление исходной высоты строки
+    row.style.height = ''; // Удаление индивидуального стиля высоты для возврата к стандартному CSS
+  }
   updateItemOnServer(id, updatedItem);
 }
+
+
 
 // Обновленная функция для отправки изменений на сервер
 function updateItemOnServer(id, updatedItem) {
@@ -214,7 +302,7 @@ function updateItemOnServer(id, updatedItem) {
     return;
   }
 
-  const url = `/api/v1/storage/tobacco/${id}/`;
+  const url = `http://188.68.221.107/api/v1/storage/tobacco/${id}/`;
   fetch(url, {
     method: 'PATCH',
     headers: {
@@ -248,33 +336,57 @@ function updateTableRow(id, updatedData) {
   if (row) {
     const cells = row.querySelectorAll('td');
 
+      // Преобразуем даты обратно в читаемый формат перед обновлением ячеек
+      const formattedPurchaseDate = formatDate(updatedData.purchase_date);
+      const formattedBestBeforeDate = formatDate(updatedData.best_before_date);
+
     // Обновляем текст в каждой ячейке, кроме первой и последней
     cells[1].innerText = updatedData.brand; // Индекс 0, потому что мы пропустили первую ячейку
     cells[2].innerText = updatedData.supplier;
     cells[3].innerText = updatedData.taste;
     cells[4].innerText = updatedData.taste_group;
-    cells[5].innerText = updatedData.purchase_date;
-    cells[6].innerText = updatedData.best_before_date;
+    cells[5].innerText = formattedPurchaseDate; // Используем отформатированную дату
+    cells[6].innerText = formattedBestBeforeDate; // Используем отформатированную дату
     cells[7].innerText = updatedData.weight;
     cells[8].innerText = updatedData.price;
-    
-    // cells[8].innerText = updatedData.organization;
-
-
     // ... обновить остальные ячейки
 
     // Возвращаем кнопки "Редактировать" и "Удалить"
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Редактировать';
-    editButton.onclick = () => startEditing(id);
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Удалить';
-    deleteButton.onclick = () => deleteItem(id);
-    
-    cells[cells.length - 1].innerHTML = '';
-    cells[cells.length - 1].appendChild(editButton);
-    cells[cells.length - 1].appendChild(deleteButton);
+    const actionCell = cells[cells.length - 1];
+    actionCell.innerHTML = `
+      <div class="action-buttons">
+        <button class="options-button" onclick="toggleEditDeleteButtons(${id})">...</button>
+        <div class="edit-delete-buttons" id="edit-delete-${id}" style="display: none;">
+          <button class="edit-button" onclick="startEditing(${id})">Редактировать</button>
+          <button class="delete-button" onclick="deleteItem(${id})">Удалить</button>
+        </div>
+      </div>
+    `;
   }
+}
+
+
+
+function cancelEdit(id) {
+  const row = document.querySelector(`tr[data-id="${id}"]`);
+  const cells = row.querySelectorAll('td');
+  // Восстановление исходных значений ячеек, если они были изменены
+  // Ваш код для восстановления исходных значений ячеек...
+  if (row) {
+    // Восстановление исходной высоты строки
+    row.style.height = ''; // Удаление индивидуального стиля высоты для возврата к стандартному CSS
+  }
+  // Восстановление исходного состояния кнопок
+  const actionCell = cells[cells.length - 1];
+  actionCell.innerHTML = `
+    <div class="action-buttons">
+      <button class="options-button" onclick="toggleEditDeleteButtons(${id})">...</button>
+      <div class="edit-delete-buttons" id="edit-delete-${id}" style="display: none;">
+        <button class="edit-button" onclick="startEditing(${id})">Редактировать</button>
+        <button class="delete-button" onclick="deleteItem(${id})">Удалить</button>
+      </div>
+    </div>
+  `;
 }
 
 // Добавим вызов startEditing в существующий обработчик событий
@@ -299,7 +411,7 @@ function deleteItem(id) {
     return;
   }
 
-  const url = `/api/v1/storage/tobacco/${id}/`;
+  const url = `http://188.68.221.107/api/v1/storage/tobacco/${id}/`;
   fetch(url, {
     method: 'DELETE',
     headers: {
@@ -342,16 +454,16 @@ function removeTableRow(id) {
 document.addEventListener('click', function(event) {
   const target = event.target;
 
-  // Открытие формы продукта
-  if (target.classList.contains('stock__btn-add')) {
-      showProductForm();
-  }
+// Открытие формы продукта
+if (event.target.closest('.stock__btn-add')) {
+  showProductForm();
+}
 
-  // Закрытие формы продукта
-  if (target.classList.contains('product-form__close') || target.classList.contains('cancel')) {
-      hideProductForm();
-      clearFormContent(); // Очистка содержимого формы
-  }
+   // Закрытие формы продукта
+   if (event.target.closest('.product-form__close') || event.target.closest('.cancel')) {
+    hideProductForm();
+    clearFormContent(); // Очистка содержимого формы
+}
 });
 
 // Функция для отображения формы продукта
@@ -445,12 +557,12 @@ document.addEventListener('DOMContentLoaded', function() {
               <input type="text" id="brand-${number}" name="brand" data-tab="${number}" placeholder="Введите наименование бренда">
           </div>
           <div class="input-group">
-      <label for="flavor-${number}">Вкус</label>
-      <input type="text" id="flavor-${number}" name="flavor" data-tab="${number}" placeholder="Введите вкус">
+      <label for="taste-${number}">Вкус</label>
+      <input type="text" id="taste-${number}" name="taste" data-tab="${number}" placeholder="Введите вкус">
   </div>
   <div class="input-group">
-  <label for="flavor-group-${number}">Группа вкуса</label>
-  <select id="flavor-group-${number}" name="flavor-group" data-tab="${number}">
+  <label for="taste_group-${number}">Группа вкуса</label>
+  <select id="taste_group-${number}" name="taste_group" data-tab="${number}">
       <option value="" disabled selected>Выберите группу вкуса</option>
       <option value="fruity">Фруктовые</option>
       <option value="berry">Ягодные</option>
@@ -468,16 +580,16 @@ document.addEventListener('DOMContentLoaded', function() {
       <input type="date" id="purchase-date-${number}" name="purchase-date" data-tab="${number}">
   </div>
   <div class="input-group">
-      <label for="expiration-date-${number}">Срок годности</label>
-      <input type="date" id="expiration-date-${number}" name="expiration-date" data-tab="${number}">
+      <label for="best_before_date-${number}">Срок годности</label>
+      <input type="date" id="best_before_date-${number}" name="best_before_date" data-tab="${number}">
   </div>
   <div class="input-group">
-      <label for="stock-remainder-${number}">Остаток на скаде, г.</label>
-      <input type="number" id="stock-remainder-${number}" name="stock-remainder" data-tab="${number}">
+      <label for="weight-${number}">Остаток на скаде, г.</label>
+      <input type="number" id="weight-${number}" name="weight" data-tab="${number}">
   </div>
   <div class="input-group">
-      <label for="purchase-price-${number}">Цена закупки, руб.</label>
-      <input type="number" id="purchase-price-${number}" name="purchase-price" data-tab="${number}">
+      <label for="price-${number}">Цена закупки, руб.</label>
+      <input type="number" id="price-${number}" name="price" data-tab="${number}">
   </div>
       `;
       formBody.appendChild(formContent);
@@ -629,6 +741,107 @@ document.addEventListener('DOMContentLoaded', function() {
   updateTabEvents();
 });
 
+// --------Отправка формы на сервер_______________
+
+document.querySelector('.save').addEventListener('click', sendAllTabsDataToServer);
+
+// Функция преобразования даты в строку ISO 8601
+function toISO8601String(dateString) {
+  const date = new Date(dateString);
+  return date.toISOString();
+}
+
+// Функция для сбора данных из всех заполненных табов
+function collectDataFromAllTabs() {
+  const allTabsData = [];
+  const allForms = document.querySelectorAll('.form-content');
+
+  allForms.forEach((form, tabIndex) => {
+    const tabData = {};
+    const inputs = form.querySelectorAll('input');
+    const selects = form.querySelectorAll('select');
+
+    inputs.forEach(input => {
+      let value = input.value;
+      if (input.type === 'number') {
+        value = parseFloat(value);
+      } else if (input.type === 'date') {
+        value = toISO8601String(value); // Убедитесь, что эта функция корректно возвращает строковое представление даты
+      }
+      let key = input.name.replace(/-/g, '_');
+      tabData[key] = value;
+    });
+
+    selects.forEach(select => {
+      let key = select.name.replace(/-/g, '_');
+      tabData[key] = select.value;
+    });
+
+    const requiredFields = ['brand', 'supplier', 'taste', 'taste_group', 'purchase_date', 'best_before_date', 'weight', 'price'];
+    let allFieldsFilled = requiredFields.every(field => {
+      const fieldValue = tabData[field];
+      if (typeof fieldValue === 'string') {
+        return fieldValue.trim() !== '';
+      } else {
+        return fieldValue !== null && fieldValue !== undefined; // Проверяем, что числовые значения заполнены (не null и не undefined)
+      }
+    });
+    
+    if (!allFieldsFilled) {
+      console.error(`Обязательное поле отсутствует или не заполнено для таба ${tabIndex + 1}`);
+      // Возможно, стоит прекратить дальнейшую обработку
+      return; // Если требуется, чтобы функция полностью остановила выполнение, используйте другой подход, например, выброс исключения
+    }
+
+    allTabsData.push(tabData);
+  });
+
+  return allTabsData;
+}
 
 
-// -----------------Табы_____________________
+// Функция отправки данных всех табов на сервер
+function sendAllTabsDataToServer() {
+  const allTabsData = collectDataFromAllTabs();
+  const authToken = localStorage.getItem('authToken');
+
+  if (!authToken) {
+    console.error('Токен аутентификации не найден.');
+    return; // Прекращаем выполнение, если токен не найден
+  }
+
+  console.log('Начало отправки данных, токен аутентификации найден.');
+
+  allTabsData.forEach((tabData, index) => {
+    console.log(`Данные для таба ${index + 1} перед отправкой:`, tabData);
+
+    tabData.organization = 1;
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(tabData),
+    };
+
+    const apiUrl = 'http://188.68.221.107/api/v1/storage/tobacco/';
+
+    fetch(apiUrl, fetchOptions)
+      .then(response => {
+        console.log(`Ответ сервера для таба ${index + 1}:`, response.status, response.statusText);
+        if (!response.ok) {
+          throw new Error(`Ошибка при отправке данных на сервер для таба ${index + 1}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(`Данные успешно отправлены для таба ${index + 1}:`, data);
+        // Обработка успешной отправки данных
+      })
+      .catch(error => {
+        console.error(`Ошибка при отправке данных на сервер для таба ${index + 1}:`, error);
+      });
+  });
+}

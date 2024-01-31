ef03f52bfd23d47601fbfcf5bc4845152cf2aee9
mikhail <mishaprizby@gmail.com>
2024-01-31 12:29:50 +0300

    Added put,patch profile

--- project/owner/serializers.py
+++ project/owner/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from owner.models import Owner
 from user.serializers import UserSerializer
 from core.serializers import SerializerUpdateMixin
+from user.models import User
 
 
 class OwnerSerializer(SerializerUpdateMixin, serializers.ModelSerializer):
--- project/owner/views.py
+++ project/owner/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .serializers import OwnerSerializer
 from .models import Owner
 from core.views import DeleteViewMixin
-
+from rest_framework.response import Response
 
 class OwnerViewSet(DeleteViewMixin, viewsets.ModelViewSet):
     queryset = Owner.objects.filter(is_active=True)
